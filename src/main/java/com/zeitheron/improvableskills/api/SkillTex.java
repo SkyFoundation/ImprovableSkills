2d66cb225f6b32a239ffd906f14cd8d572790a3c
Zeitheron <zeitheron.dr@gmail.com>
2018-07-02 15:15:26 +0300

    Fix startup crash

--- src/main/java/com/zeitheron/improvableskills/api/SkillTex.java
+++ src/main/java/com/zeitheron/improvableskills/api/SkillTex.java
@@ -9,19 +9,23 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 public class SkillTex
 {
 	public final PlayerSkillBase skill;
-	public final ResourceLocation texNorm, texHov;
+	public ResourceLocation texNorm, texHov;
 	
 	public SkillTex(PlayerSkillBase skill)
 	{
 		this.skill = skill;
-		ResourceLocation res = skill.getRegistryName();
-		this.texNorm = new ResourceLocation(res.getResourceDomain(), "textures/skills/" + res.getResourcePath() + "_normal.png");
-		this.texHov = new ResourceLocation(res.getResourceDomain(), "textures/skills/" + res.getResourcePath() + "_hovered.png");
 	}
 	
 	@SideOnly(Side.CLIENT)
 	public UV toUV(boolean hovered)
 	{
+		if(texHov == null || texNorm == null)
+		{
+			ResourceLocation res = skill.getRegistryName();
+			this.texNorm = new ResourceLocation(res.getResourceDomain(), "textures/skills/" + res.getResourcePath() + "_normal.png");
+			this.texHov = new ResourceLocation(res.getResourceDomain(), "textures/skills/" + res.getResourcePath() + "_hovered.png");
+		}
+		
 		return new UV(hovered ? texHov : texNorm, 0, 0, 256, 256);
 	}
 }
